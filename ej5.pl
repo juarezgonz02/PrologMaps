3b13236856b78f43aa7548c027337696d37525e8
VictorCortez358 <00022120@uca.edu.sv>
2023-09-25 09:27:50 -0600

    add parques, gasolineras,centroComerciales

--- ej5.pl
+++ ej5.pl
@@ -4,3 +4,75 @@ lugar('Cafetalon', 13.676566396794092, -89.2833433105481, parque).
 lugar('Climosal', 13.675123700882084, -89.29010906898763, salud).
 lugar('Colegio Fatima', 13.67130270093074, -89.28404001848129, educacion).
 
+% Parques, eco parques, zonas verdes, canchas, plazas, complejos deportivos
+
+lugar('Parque Refugio de La Paz', 13.679852273915275, -89.29148434548138).
+lugar('Parque Daniel Hernández', 13.673680991582346, -89.28872366264007).
+lugar('Parque San Martín', 13.673488136403295, -89.2852592763294).
+lugar('Parque de la Familia', 13.682111676728264, -89.28247378319311).
+lugar('Parque Refugio de La Paz', 13.679892806611054, -89.2914445729038).
+lugar('Parque Colonia Monte Sión', 13.682851282566574, -89.29035293037498).
+lugar('Zona Verde Colonia Las Palmeras', 13.685288162230508, -89.29374063107875).
+lugar('Parque Residencial San Rafael', 13.681510555972425, -89.29816035473327).
+lugar('Parque Residencial Peña Blanca',13.6824026956423, -89.29782763585608).
+lugar('Plaza Tegucigalpa', 13.678157046818074, -89.29861742554162).
+lugar('Parque deportivo El Cafetalón',13.676274109766872, -89.2832722506821).
+lugar('Complejo Deportivo Banco Cuscatlán', 13.674066893544916, -89.27100661072917).
+lugar('Cancha y Parque Jardines de la Libertad', 13.673133877451818, -89.26788988393419).
+lugar('Parque Las Colinas', 13.668436932634878, -89.28773539884874).
+lugar('Zona verde residencial el paraíso', 13.668145032755575, -89.29010647150444).
+lugar('Parque Residencial Altos de Santa Monica', 13.684995918186157, -89.28377359337372).
+lugar('Parque de Polígono G, Residencial Finca de Asturias', 13.682793783762069, -89.28033366041228).
+lugar('Cancha La Lobos',13.68382339910677, -89.27211249095504).
+lugar('Parque Extremo Merliot', 13.683124328473832, -89.26614679946252).
+lugar('Area Verde Residencial Irlanda', 13.669358753665657, -89.29143571008632).
+lugar('Estadio Nacional Las Delicias',13.672524007427404, -89.29985225203939).
+
+% Gasolineras
+
+lugar('UNO', 13.673578103771643, -89.30026948228978).
+lugar('Puma Energy', 13.673758640830359, -89.29810936129101).
+lugar('TEXACO • RV Santa Tecla', 13.67233440615656, -89.29746441551245).
+lugar('UNO • Utila', 13.663671879852798, -89.2771208013784).
+lugar('Food Mart • TEXACO SERVITEX', 13.671872056756083, -89.27582206428401).
+lugar('Puma Energy', 13.67278493675931, -89.27945607729707).
+lugar('DLC • Santa Tecla', 13.671753858151131, -89.28181289850966).
+lugar('UNO • Santa Tecla', 13.671846393912473, -89.28349606971955).
+lugar('TEXACO • La Skina', 13.678196934899196, -89.29681977686693).
+lugar('Estacion de Servicio Iraheta', 13.670576514842393, -89.29374217992327).
+lugar('UNO • Merliot',13.679645379533246, -89.27740226761277).
+lugar('TEXACO • Merliot', 13.679249248463135, -89.27806745541665).
+
+% Centros comerciales
+
+lugar('Plaza Merliot',13.67877495811841, -89.27606475597719).
+lugar('Plaza Santa Fe', 13.677601486024447, -89.27725464115977).
+lugar('Plaza El Cafetalón', 13.676526448368563, -89.28361751149689).
+lugar('Plaza Santa Mónica', 13.679943017238166, -89.28541043261065).
+lugar('Centro Comercial El Gran Bazar', 13.679249297522693, -89.26387818958837).
+lugar('Centro Comercial La Sabana', 13.681358215431388, -89.27035834645521).
+lugar('Centro Comercial Santa Rosa', 13.678304959243917, -89.30060957924357).
+lugar('Centro Comercial Holanda', 13.673767120154867, -89.29424054736297).
+lugar('Centro Comercial Las Ramblas',13.671782528757827, -89.26975558530522).
+lugar('Solusersa El Salvador', 13.679225514447714, -89.26387874120556).
+lugar('Plaza El Carmen', 13.674897268310067, -89.28822264399926).
+lugar('Plaza Serrano', 13.675105762283856, -89.28903267108298).
+lugar('Parqueo Portal Plaza',13.67287486714642, -89.28918287479387).
+lugar('Accesorios S&R', 13.673242340806533, -89.28772107095367).
+lugar('RinconGeek', 13.67245005644105, -89.29155394754623).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
